eff3640269278a4122f9582342d1b3495f5ce378
Matt Sieker <matt@siekernet.com>
2014-11-28 22:11:26 +0000

    - Update gitignore for pyc

--- monitor.py
+++ monitor.py
@@ -1,6 +1,20 @@
+#!/usr/bin/env python
+import sys
+from twisted.internet import reactor
+from twisted.python import log
 
-def main():
-    pass
+from configuration import Configuration
+from weatherlogger import WeatherLogger
+
+def Main():
+    def __init__(self):
+        self.config = Configuration()
+
+    def start():
+        log.startLogging(sys.stdout)
+        reactor.run()
+
 
 if __name__=="__main__":
-    main()
+    main = Main()
+    main.start()

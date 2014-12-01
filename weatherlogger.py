14db32be28bc244cb38a7f5308e7011727ac692d
Matt Sieker <matt@siekernet.com>
2014-12-01 02:51:03 +0000

    Have weather logger write out to redis

--- weatherlogger.py
+++ weatherlogger.py
@@ -1,6 +1,11 @@
 from twisted.internet import task, reactor, defer, protocol
 from twisted.web.client import Agent
 
+from redisclient import RedisClientWrapper
+from storenames import Stores
+import json
+
+
 class ResponseReader(protocol.Protocol):
     def __init__(self, finished):
         self.finished = finished
@@ -13,36 +18,46 @@ class ResponseReader(protocol.Protocol):
         
 
 class WeatherLogger():
+
     def __init__(self, settings):
         self.settings = settings
-        
-        interval = self.settings['weather']['interval']
+        self.redis = RedisClientWrapper(self.settings)
+
+        self.interval = self.settings['weather']['interval']
         
         self.apikey = self.settings['weather']['apikey']
         self.latitude = self.settings['weather']['latitude']
         self.longitude = self.settings['weather']['longitude']
 
+        self._ConnectToRedis()
+
+    @defer.inlineCallbacks
+    def _ConnectToRedis(self):
+        yield self.redis.Connect()
         self.agent = Agent(reactor)
         self.monitortask = task.LoopingCall(self.UpdateWeather)
-        self.monitortask.start(interval)
+        self.monitortask.start(self.interval)
 
+    @defer.inlineCallbacks
     def UpdateWeather(self):
         url = 'https://api.forecast.io/forecast/{0}/{2},{1}?exclude=minutely,hourly,daily'.format(self.apikey, self.latitude, self.longitude)
         print "Updating Weather from", url
-        d = self.agent.request('GET',url)
-        d.addCallback(self.GotResponse)
-
-    def GotResponse(self, response):
+        response = yield self.agent.request('GET',url)
+        
         if response.code == 200:
-            d = defer.Deferred()
-            response.deliverBody(ResponseReader(d))
-            d.addCallback(self.ProcessBody)
-            return d
+            body = yield self._GetBody(response)
+            data = json.loads(body)
+            current = data['currently']
+            print Stores.weather
+            key = yield self.redis.AddDict(Stores.weather.value, current)
+            yield self.redis.AddToTimeSeries(Stores.weather.value, key)
+            yield self.redis.PublishKey(Stores.weather.value, key)
 
-    def ProcessBody(self, body):
-        print body
-        
-        
+    def _GetBody(self, response):
+        finished = defer.Deferred()
+        response.deliverBody(ResponseReader(finished))
+        return finished
+
 if __name__=="__main__":
     from configuration import Configuration
     conf = Configuration()
